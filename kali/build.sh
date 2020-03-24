bc5387be522605bc30910b0337ceda5c6739f971
libertyunix <2050273+LibertyUnix@users.noreply.github.com>
2020-03-24 14:58:23 -0400

    Update build.sh

--- kali/build.sh
+++ kali/build.sh
@@ -1,3 +1,4 @@
+export DEBIAN_FRONTEND=noninteractive
 echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
 gpg --keyserver pgpkeys.mit.edu --recv-key  ED444FF07D8D0BF6
 gpg -a --export ED444FF07D8D0BF6 | sudo apt-key add -
