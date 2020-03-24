7d67bae3994492fac554390e4f82b9c1420a5491
libertyunix <2050273+LibertyUnix@users.noreply.github.com>
2020-03-24 13:52:04 -0400

    Update build.sh

--- kali/build.sh
+++ kali/build.sh
@@ -4,3 +4,19 @@ gpg -a --export ED444FF07D8D0BF6 | sudo apt-key add -
 apt-get -y update
 apt-get -y dist-upgrade
 sleep 5
+#!/bin/bash
+echo "SYSTEM UPDATE"
+export DEBIAN_FRONTEND=noninteractive
+add-apt-repository universe
+sleep 5
+apt-get -y update
+sleep 30
+apt-get upgrade -yq
+sleep 30
+apt-get -y dist-upgrade
+sleep 30
+apt-get install -y expect
+useradd libertyunix -p password
+usermod -aG admin libertyunix 
+usermod -aG sudo libertyunix
+bash /root/gui.sh
